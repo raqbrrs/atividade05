2f5e0e9eba4604e90f54f4405e62aead4f11b16f
raqbrrs <raquelbrrs07@gmail.com>
2024-09-04 16:05:46 -0300

    conversao de min para seg

---
+++ atividade05.py
@@ -0,0 +1,7 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+
+minuto = int(input("digite o valor em min"))
+segundo = 60 
+
+conversao = minuto * segundo 
+print (conversao)
